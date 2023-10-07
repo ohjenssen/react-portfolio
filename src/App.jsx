1fe925c9bcc01519901d926843489ff556390c2f
ohjenssen <oskar_jenssen@hotmail.com>
2023-10-07 17:09:43 +0200

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+        <button className='btn btn-primary'>Hello</button>
+        <button className='btn btn-danger'>Danger</button>
+    </>
+  )
+}
+
+export default App
